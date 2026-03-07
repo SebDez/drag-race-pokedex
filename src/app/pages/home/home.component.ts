3a6f4841ce7ef154444227602607cfd5a9b39631
SebDez <sebdez.dev@gmail.com>
2026-03-07 15:07:44 +0100

    refactor(table): remove loading directive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,33 +1,19 @@
-import { Component, inject, OnInit, signal, computed } from '@angular/core';
+import { Component, inject, OnInit, computed } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ContestantsStore } from '../../store/contestants.store';
 import { ContestantListComponent } from '../../components/contestant-list/contestant-list.component';
-import { ObserveVisibleDirective } from '../../shared/observe-visible.directive';
-
-const PAGE_SIZE = 96;
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, ContestantListComponent, ObserveVisibleDirective],
+  imports: [TranslateModule, ContestantListComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   protected readonly store = inject(ContestantsStore);
 
-  private readonly contestantsVisibleCount = signal(PAGE_SIZE);
-
-  protected readonly visibleContestants = computed(() =>
-    this.store.contestants().slice(0, this.contestantsVisibleCount()),
-  );
-
-  protected readonly hasMore = computed(() => this.contestantsVisibleCount() < this.store.count());
+  protected readonly visibleContestants = computed(() => this.store.contestants());
 
   ngOnInit(): void {
     this.store.loadContestants();
-  }
-
-  protected loadMore(): void {
-    this.contestantsVisibleCount.update((n) => Math.min(n + PAGE_SIZE, this.store.count()));
   }
 }
